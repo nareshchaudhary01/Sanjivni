43a12274ca938481bf9c974ba652c4d9c94b0a9e
Naresh Chaudhary <nareshchaudhary1532@gmail.com>
2026-07-17 20:43:35 +0530

    Configure Django templates and render home page

--- website/views.py
+++ website/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to The Moringa Co.")
+    return render(request, 'home.html')
 
 # Create your views here.

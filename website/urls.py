7e4d2132eed0c98d53cbdacb93657a77eeb546ac
Naresh Chaudhary <nareshchaudhary1532@gmail.com>
2026-07-17 21:20:26 +0530

    Add basic website pages and URL routing

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("menu/", views.menu, name="menu"),
+    path("contact/", views.contact, name="contact"),
 ]
+
--- website/views.py
+++ website/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
+def about(request):
+    return render(request, "about.html")
+
+def menu(request):
+    return render(request, "menu.html")
+
+def contact(request):
+    return render(request, "contact.html")
 # Create your views here.
